405558791d8081fd1276f6076007e2accd18ccf4
Dmytro <dimusgamer2.0@gmail.com>
2025-12-07 04:21:26 +0200

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const splittedStyles = sourceString
+    .split(';')
+    .filter((style) => style.trim());
+
+  const objectStyles = splittedStyles.reduce((acc, style) => {
+    const [styleName, styleValue] = style.split(':').map((str) => str.trim());
+
+    if (styleName && styleValue) {
+      acc[styleName] = styleValue;
+    }
+
+    return acc;
+  }, {});
+
+  return objectStyles;
 }
 
 module.exports = convertToObject;
